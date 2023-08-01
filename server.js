aa6d63ede0305888f2a45c182d1c1498171bebd5
pavantejasaikam07 <pavantejasaikam07@gmail.com>
2023-08-01 22:48:20 +0530

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
